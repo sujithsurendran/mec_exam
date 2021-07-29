0efafc254118620c614396fbcffd0ebb7213bc28
Sujith <sujithsurendran@gmail.com>
2021-07-30 01:19:45 +0530

    ....working

--- format_file_002.py
+++ format_file_002.py
@@ -1,42 +1,54 @@
 #format_file
 #from MyFunctions_001 import *
 
-
-
 #def fetch_rooms():
-students = 30
-details=[]
-rooms=[]
-capacities=[]
-max_capacities=[]
-current_capacities=[]
-i=1
-with open("./rooms.txt") as file1:
-	for line in file1:
-		line_i = line.rstrip()
-		details = line_i.split(",")
-		rooms.append(details[0])
-		capacities.append(details[1])
-		max_capacities.append(details[2])
-		current_capacities.append(details[3])
+while True:
+	students = 30
+	students = int(input("Students"))
+	alter = int(input("Alteration?"))
+	if alter == "":
+		alter = 0
 		
-		i = i + 1
-	#return rooms, capacities, max_capacities, current_capacities
+	details=[]
+	rooms=[]
+	capacities=[]
+	max_capacities=[]
+	current_capacities=[]
 
-#rooms, capacities,max_capacities,current_capacities = fetch_rooms()
+	total = 0
+	with open("./rooms.txt") as file1:
+		for line in file1:
+			line_i = line.rstrip()
+			details = line_i.split(",")
+			rooms.append(details[0])
+			
+			max_capacities.append(details[2])
+			current_capacities.append(details[3])
 
-i=0
-total=0
-for room,capacity,max_capacity,current_capacity in zip(rooms,capacities,max_capacities,current_capacities):
-		print(str(room) + "=>" + str(capacity) )
-		total = total + int(capacity)
-		if total>=students:
-			print("Students = " + str(students) + "Total = " + str(total) + ", Balance = " + str(students - total))
-			exit()
-		#print(capacity)
-		#print(max_capacity)
-		#print(current_capacity)
+			capacity = int(details[1]) + alter
+			
+			if capacity >= students:
+				capacities.append(students)
+				for room1, capacity1 in zip(rooms,capacities):
+					print(room1 + "=>" + str(capacity1))
+
+				exit()
+			else:
+				#print(capacities)
+				#print(rooms)
+				if students>=0:
+					students = students - capacity 
+					capacities.append(capacity)
+					print(students)
+				else:
+					exit()
 
 		
+			
 		
 		
+
+
+
+	#for room,capacity,max_capacity,current_capacity in zip(rooms,capacities,max_capacities,current_capacities):
+	
